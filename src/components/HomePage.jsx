ec34f248dedf1ad3438ab4be90668acb3528b8f3
DipWorks <63048134+DipWorks@users.noreply.github.com>
2021-03-10 21:43:13 +1100

    adding studio files

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+//import background from "../../public/images/theme.jpg";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div style={{ backgroundImage: `url("images/theme1.jpg")`, padding: 50 }}>
+        Dipendra Bhandari
+      </div>
+    );
+  }
+}
+
+export default HomePage;

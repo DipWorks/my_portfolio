6a4a6f2db6085aff884c55182a0bf5e3c81cd3a9
DipWorks <63048134+DipWorks@users.noreply.github.com>
2021-03-16 17:06:03 +1100

    fixing no key error in prorject.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -30,8 +30,11 @@ function Project(props) {
         </h2>
         <section className="grid grid-cols-2 gap-8">
           {projectData &&
-            projectData.map((project) => (
-              <article className="relative rounded-lg shadow-xl bg-white p-16">
+            projectData.map((project, index) => (
+              <article
+                key={index}
+                className="relative rounded-lg shadow-xl bg-white p-16"
+              >
                 <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
                   <a
                     href={project.link}

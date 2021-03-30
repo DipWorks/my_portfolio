f48f18c3bd000dcaf4dd239e699eea45c8d9b2b2
DipWorks <63048134+DipWorks@users.noreply.github.com>
2021-03-30 13:48:03 +1100

    improving blog posts page

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
+import backgroundImage from "../images/workhard.jpg";
 
 function Post(props) {
   const [postData, setPost] = useState(null);
@@ -30,18 +31,24 @@ function Post(props) {
   }, []);
 
   return (
-    <main className="bg-blue-300 border-8 border-indigo-300 rounded-3xl min-h-screen p-12">
+    <main className="bg-blue-300">
+      <img
+        src={backgroundImage}
+        alt="work desk, work hard message"
+        className="absolute object-cover w-full h-full border-8 border-indigo-300 rounded-3xl"
+      />
+
       {/* mx-auto centers the container
       container class sets the max-width of the element to match the min-width of the 
       current breakpoint */}
-      <section className="container mx-auto">
+      <section className="container relative mx-auto p-5 lg:p-10">
         <h1 className="text-5xl text-yellow-600 flex justify-center cursive">
           Blog Posts
         </h1>
         <h2 className="text-lg text-red-500 font-bold text-gray-600 flex justify-center mb-12 ">
           Welcome to my blog posts
         </h2>
-        <div className="grid md: grid-cols-2 lg: grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {postData &&
             postData.map((post, index) => (
               <article key={index}>
